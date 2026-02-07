0d6ef3fa30eb6baf714ce5373d8cb2b7d7d9f6aa
salamb1 <salambouso24@gmail.com>
2026-02-07 16:39:06 -0600

    country html files

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import StartScreen from "./OtherPages/StartScreen.jsx";
-import WorldMap from "./OtherPages/WorldMap/WorldMap.jsx";
-
-export default function App() {
-  const [started, setStarted] = useState(false);
-
-  return <>{started ? <WorldMap /> : <StartScreen onStart={() => setStarted(true)} />}</>;
-}
--- salam/script.js
+++ salam/script.js
@@ -1,10 +1,10 @@
 document.querySelectorAll("svg path").forEach(country => {
     country.addEventListener("click", () => {
         const countryName = country.getAttribute("class");
-        
+        localStorage.setItem('Country', country);
         // Redirect to a new page (for example, country.html) with the country name as a query parameter
         if (countryName) {
-            window.location.href = `country.html?name=${encodeURIComponent(countryName)}`;
+            window.location.href = `${countryName}.html?name=${encodeURIComponent(countryName)}`;
         }
     });
 });
